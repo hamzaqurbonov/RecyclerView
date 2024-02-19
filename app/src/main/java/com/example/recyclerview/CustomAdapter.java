27555d041f6301193425802bd2be6e2d0419ef97
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-02-19 12:32:33 +0500

    recyclerView
    Кодлар тўғирланиб енгиллаштирилди

--- app/src/main/java/com/example/recyclerview/CustomAdapter.java
+++ app/src/main/java/com/example/recyclerview/CustomAdapter.java
@@ -1,36 +1,22 @@
 package com.example.recyclerview;
 
-import android.content.Context;
-import android.content.Intent;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
-
 import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private MainActivity activity;
     private RecyclerViewClickListner listner;
-
-    //    private Context context;
     public List<Model> modellist;
 
-
     public CustomAdapter(MainActivity activity, List<Model> modellist, RecyclerViewClickListner listner) {
-//        this.context=context;
         this.modellist=modellist;
-        this.activity=activity;
         this.listner=listner;
     }
 
@@ -44,37 +30,15 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        Model member = modellist.get(position);
 
+        Model member = modellist.get(position);
         if(holder instanceof CustomViewHolder){
-            LinearLayout lay_click = ((CustomViewHolder) holder).lay_click;
+
             TextView fist_name = ((CustomViewHolder) holder).first_name;
             TextView last_name = ((CustomViewHolder) holder).last_name;
 
             fist_name.setText(member.getFirstName());
             last_name.setText(member.getLastName());
-
-//            lay_click.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    activity.openItem(member);
-//                    activity.initYouTubePlayerView();
-//                    Intent intent = new Intent(v.getContext(), MainActivity2.class);
-//                    v.getContext().startActivity(intent);
-
-//                    ((CustomViewHolder) holder).youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-//                        @Override
-//                        public void onReady(@NonNull YouTubePlayer youTubePlayer)  {
-//
-//                            String videoId = "HXrETVPKWh0";
-//                            youTubePlayer.cueVideo(videoId, 0);
-////                Log.d("demo20", String.valueOf(2));
-////
-//                        }
-//
-//                    });
-//                }
-//            });
         }
 
     }
@@ -86,17 +50,13 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     public class CustomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public View view;
-        private LinearLayout lay_click;
-        YouTubePlayerView youTubePlayerView;
-        public TextView first_name, last_name, nameTxt;
+        public TextView first_name, last_name;
         public CustomViewHolder(View v) {
             super(v);
             view = v;
-//            lay_click = view.findViewById(R.id.lay_click);
+
             first_name = view.findViewById(R.id.first_name);
             last_name = view.findViewById(R.id.last_name);
-//            youTubePlayerView = view.findViewById(R.id.youtube_player_view);
-//            nameTxt = view.findViewById(R.id.nameTextView);
             view.setOnClickListener(this);
         }
 
@@ -106,17 +66,10 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         }
     }
 
-
     public interface RecyclerViewClickListner {
         void onClick(View v, int position);
     }
-//    public class MyViewHolder extends RecyclerView.ViewHolder {
-//        private TextView nameTxt;
-//        public MyViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//
-//        }
-//    }
+
 
 
 }
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,74 +1,56 @@
 package com.example.recyclerview;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.YouTubePlayerUtils;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
-
-import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    YouTubePlayerView youTubePlayerView;
     List<Model> modellist = new ArrayList<>();
-
-    private Context context;
     private RecyclerView recyclerView;
     private CustomAdapter.RecyclerViewClickListner listner;
-    Button playNextVideoButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
         initViews();
-        List<Model> modellist = prepareMemerList();
+        prepareMemerList();
+        setOnClickListner();
         refreshAdapter(modellist);
-
-
     }
-
 
     private void initViews() {
-        context = this;
         recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(context, 1));
-
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
     }
 
-    private void refreshAdapter (List<Model>modellist) {
-        setOnClickListner();
-        CustomAdapter adapter = new CustomAdapter(this, modellist,listner);
+
+    private void refreshAdapter(List<Model> modellist) {
+        CustomAdapter adapter = new CustomAdapter(this, modellist, listner);
         recyclerView.setAdapter(adapter);
     }
 
+    private List prepareMemerList() {
+        modellist.add(new Model("Kurbanov", "HXrETVPKWh0"));
+        modellist.add(new Model("Kurbanov", "X3tr5ax78V4"));
+        modellist.add(new Model("Kurbanov", "741"));
+
+//        for (int i = 0; i<=5; i++) {
+//            modellist.add(new Model("Kurbanov " + i, "Hamza " + i));
+//        }
+        return modellist;
+    }
+
     private void setOnClickListner() {
-//        Log.d("demo15", );
         listner = new CustomAdapter.RecyclerViewClickListner() {
             @Override
             public void onClick(View v, int position) {
@@ -79,21 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
         };
 
-    }
-
-
-
-    private List<Model> prepareMemerList() {
-
-        modellist.add(new Model("Kurbanov", "HXrETVPKWh0"));
-        modellist.add(new Model("Kurbanov", "X3tr5ax78V4"));
-        modellist.add(new Model("Kurbanov", "k_an7b4r1_Q"));
-
-//        for (int i = 0; i<=5; i++) {
-//            modellist.add(new Model("Kurbanov " + i, "Hamza " + i));
-//
-//        }
-        return modellist;
     }
 
 
