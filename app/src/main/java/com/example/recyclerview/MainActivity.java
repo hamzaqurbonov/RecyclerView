a0326bdae6d641dbde90e2a056f036b4bf71f30d
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-10-17 21:02:07 +0500

    Havfsiz bolajon
    Havfsiz bolajonga ulandi. Ishladi

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -29,18 +29,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    public  static ArrayList<String> nextArrayList = new ArrayList<>();
+    String oldID;
     private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt, idTestEdit;
     private Button addCourseBtn, readCourseBtn, shareSend;
     private DBHandler dbHandler;
@@ -61,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
 
 //        courseNameEdt = findViewById(R.id.idEdtCourseName);
 //        courseTracksEdt = findViewById(R.id.idEdtCourseTracks);
@@ -159,9 +172,87 @@ public class MainActivity extends AppCompatActivity {
         setUpRecyclerView();
 
 
+        nextArrayList();
+
+    }
+
+
+    private void nextArrayList() {
+        // Birinchi holati ishlaydi Arreyni shakillantirib beradi
+
+        db.collection("Shorts").orderBy("key").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                        ArrayList<String> arrayMapList = (ArrayList<String>) document.get("key");
+                        for (Object transaction: arrayMapList) {
+                            Map values = (Map)transaction;
+                            nextArrayList.add((String) values.get("id"));
+                        }
+
+
+                    }
+                    Log.d("demo47", "Fragment ID: " + nextArrayList);
+                    Collections.shuffle( nextArrayList);
+                } else {
+                }
+            }
+        });
+        Log.d("demo47", "Found: ");
+        db.collection("Shorts").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+//                            nextArrayList.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // "key" массивини оламиз
+                                List<Map<String, Object>> keys = (List<Map<String, Object>>) document.get("key");
+
+                                // Массив ичидаги youngNumber бўйича фильтрлаш
+                                for (Map<String, Object> key : keys) {
+//                                    long youngNumber = (long) key.get("youngNumber"); // youngNumberни оламиз
+
+//                                    if (youngNumber ==  2) { // youngNumber 0 дан катта бўлса
+//                                        Log.d("demo47", "Found: " + key.get("id")); // Мос келганларини логга чиқарамиз
+//                                    }
+                                    Object youngNumberObj = key.get("youngNumber");
+                                    if (youngNumberObj != null && !youngNumberObj.toString().isEmpty()) {
+                                        try {
+                                            // Агар youngNumber String бўлса, уни long га айлантирамиз
+                                            long youngNumber = Long.parseLong(youngNumberObj.toString());
+
+                                            if (youngNumber == Integer.parseInt(oldID)) {
+
+                                                nextArrayList.add((String) key.get("id"));
+                                                Log.d("demo47", "Found: " + key.get("id"));
+                                            }
+                                        } catch (NumberFormatException e) {
+//                                            Log.e("demo47", "Маълумотни long га айлантиришда хато: " + youngNumberObj.toString());
+                                        }
+                                    } else {
+                                        Log.w("demo47", "youngNumber null ёки бўш, текширинг: " + key.toString());
+                                    }
+
+
+                                }
+                            }
+                            Collections.shuffle( nextArrayList);
+                        } else {
+                            Log.w("demo47", "Маълумот олишда хато юз берди.", task.getException());
+                        }
+                    }
+                });
 
 
     }
+
+
+
+
+
 
 
 
